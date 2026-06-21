5cc85dbbac97c86b9101cec2613a96bce4022b1e
hailuworku <234903647+hailuworku@users.noreply.github.com>
2026-06-21 20:19:52 +0300

    Configure TmsDbContext and Apply the First Migration

---
+++ Data/TmsDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TmsApi.Entities;
+namespace TmsApi.Data;
+
+public class TmsDbContext(DbContextOptions<TmsDbContext> options) : DbContext(options)
+{
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+    //remenber
+    //join tables
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Enrollment>()
+            .HasOne(e => e.Student)
+            .WithMany(s => s.Enrollments)
+            .HasForeignKey(e => e.StudentId);
+
+        modelBuilder.Entity<Enrollment>()
+            .HasOne(e => e.Course)
+            .WithMany(c => c.Enrollments)
+            .HasForeignKey(e => e.CourseId);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,16 +2,16 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.EntityFrameworkCore;
+using TmsApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<TmsDbContext>(options
+ => options.UseNpgsql(builder.Configuration.GetConnectionString("TmsDatabase")));
 // Add authentication and authorization services
 builder.Services.AddAuthentication("Training")
     .AddScheme<AuthenticationSchemeOptions, TrainingAuthHandler>("Training", null);
 builder.Services.AddAuthorization();
-
-
 var app = builder.Build();
 // Use the request logging middleware
 app.UseMiddleware<RequestLoggingMiddleware>();
