8f0f5f2e3e79ebdfc6d918ba2eee99d5d59891ca
hailuworku <234903647+hailuworku@users.noreply.github.com>
2026-07-23 04:38:20 +0300

    Add Course CRUD with validation and API versioning

--- TmsApi.Domain/obj/Debug/net10.0/TmsApi.Domain.AssemblyInfo.cs
+++ TmsApi.Domain/obj/Debug/net10.0/TmsApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ec4189eb93bf23981236ceb0a56eb74cc12813")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
+++ TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ec4189eb93bf23981236ceb0a56eb74cc12813")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
